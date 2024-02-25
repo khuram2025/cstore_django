6cf24be2ecaa33ebe50061717e700bf2bde9bb39
khuram2025 <khuram2025@gmail.com>
2024-02-25 21:51:53 +0300

    login sighnup working

---
+++ accounts/api_views.py
@@ -0,0 +1,43 @@
+from django.contrib.auth import authenticate
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from .serializers import CustomUserSerializer, LoginSerializer, SignupSerializer
+from rest_framework.permissions import IsAuthenticated
+from .serializers import LoginSerializer
+from rest_framework.authtoken.models import Token
+
+class LoginAPIView(APIView):
+    def post(self, request):
+        serializer = LoginSerializer(data=request.data)
+        if serializer.is_valid():
+            mobile = serializer.validated_data['mobile']
+            password = serializer.validated_data['password']
+            user = authenticate(mobile=mobile, password=password)
+            if user is not None:
+                token, created = Token.objects.get_or_create(user=user)
+                return Response({'token': token.key}, status=status.HTTP_200_OK)
+            else:
+                return Response({'message': 'Invalid credentials'}, status=status.HTTP_401_UNAUTHORIZED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+
+
+class SignupAPIView(APIView):
+    def post(self, request):
+        serializer = SignupSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response({'message': 'Signup successful'}, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class HomeView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request):
+        # Serialize the logged in user's data
+        serializer = CustomUserSerializer(request.user)
+        serialized_data = serializer.data
+        print("Serialized Data:", serialized_data)
+        return Response(serializer.data)
---
+++ accounts/models.py
@@ -0,0 +1,45 @@
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
+from django.db import models
+
+class CustomUserManager(BaseUserManager):
+    def create_user(self, mobile, password=None, **extra_fields):
+        if not mobile:
+            raise ValueError('The Mobile Number must be set')
+        user = self.model(mobile=mobile, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, mobile, password, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError('Superuser must have is_staff=True.')
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError('Superuser must have is_superuser=True.')
+
+        return self.create_user(mobile, password, **extra_fields)
+
+class CustomUser(AbstractBaseUser, PermissionsMixin):
+    mobile = models.CharField(max_length=15, unique=True)
+    name = models.CharField(max_length=50, blank=True)
+    business_name = models.CharField(max_length=100)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+    objects = CustomUserManager()
+
+    USERNAME_FIELD = 'mobile'
+    REQUIRED_FIELDS = ['business_name']
+
+    def __str__(self):
+        return self.mobile
+
+
+class Business(models.Model):
+    name = models.CharField(max_length=100)
+    categories = models.CharField(max_length=200, blank=True)  # A simple string for now
+
+    def __str__(self):
+        return self.name
