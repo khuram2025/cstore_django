11b94fdeee4086326ee0993d6edd477ffd5596c0
khuram2025 <khuram2025@gmail.com>
2024-03-02 20:04:43 +0300

    aaa

--- customers/serializers.py
+++ customers/serializers.py
@@ -1,6 +1,6 @@
 from accounts.models import Business
 from rest_framework import serializers
-from .models import Customer
+from .models import Customer, Transaction
 
 class CustomerSerializer(serializers.ModelSerializer):
     businesses = serializers.PrimaryKeyRelatedField(many=True, required=False, queryset=Business.objects.all())
@@ -8,3 +8,11 @@ class CustomerSerializer(serializers.ModelSerializer):
     class Meta:
         model = Customer
         fields = ['id', 'name', 'phone', 'businesses']
+
+
+
+
+class TransactionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Transaction
+        fields = ['customer_account', 'date', 'time', 'amount', 'transaction_type', 'notes', 'attachment']
