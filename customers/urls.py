f06413f8116a75bdc8dc4b543e9a5525427e7915
khuram2025 <khuram2025@gmail.com>
2024-03-04 14:45:19 +0300

    Trasaction add and list working properly

--- customers/urls.py
+++ customers/urls.py
@@ -4,8 +4,9 @@ from . import api_views
 
 urlpatterns = [
     # ... other URL patterns ...
-    path('api/customers/', api_views.CustomerListView.as_view(), name='customer-list'),
+    
     path('api/customer-create-link/', api_views.CustomerCreateLinkView.as_view(), name='customer-create-link'),
     path('api/add-transaction/', api_views.AddTransactionView.as_view(), name='add-transaction'),
+    path('api/transactions/<int:customer_account_id>/', api_views.TransactionListView.as_view(), name='transaction-list'),
     path('api/customer-accounts/', api_views.CustomerAccountListView.as_view(), name='customer-account-list'),  
 ]
