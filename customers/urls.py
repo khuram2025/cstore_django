baed353d62e1cbb7d1b58b3d13a2bb75f7f78530
khuram2025 <khuram2025@gmail.com>
2024-03-04 10:13:58 +0300

    Add Trasaction working

--- customers/urls.py
+++ customers/urls.py
@@ -6,9 +6,6 @@ urlpatterns = [
     # ... other URL patterns ...
     path('api/customers/', api_views.CustomerListView.as_view(), name='customer-list'),
     path('api/customer-create-link/', api_views.CustomerCreateLinkView.as_view(), name='customer-create-link'),
-    path('api/add-transaction/', api_views.AddTransactionAPIView.as_view(), name='add-transaction'),
-    path('api/customer-accounts/', api_views.CustomerAccountListView.as_view(), name='customer-account-list'),
-
-
-   
+    path('api/add-transaction/', api_views.AddTransactionView.as_view(), name='add-transaction'),
+    path('api/customer-accounts/', api_views.CustomerAccountListView.as_view(), name='customer-account-list'),  
 ]
