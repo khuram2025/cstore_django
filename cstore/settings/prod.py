16f8945ce3a94f1a3cd29ffe2a901fd7e5074545
khuram2025 <khuram2025@gmail.com>
2024-06-11 05:26:47 +0300

    prodchanges

--- cstore/settings/prod.py
+++ cstore/settings/prod.py
@@ -1,32 +1,37 @@
 from .base import *
+from pathlib import Path
+import os
 
-DEBUG = False
+# Build paths inside the project like this: BASE_DIR / 'subdir'.
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
-ALLOWED_HOSTS = ['yourdomain.com', 'www.yourdomain.com']
+# Database
+# https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'channab',
-        'USER': 'khan',
-        'PASSWORD': 'C@hannb183!',
-        'HOST': 'localhost',
-        'PORT': '',
+        'NAME': os.getenv('DB_NAME', 'channabdb'),
+        'USER': os.getenv('DB_USER', 'channab'),
+        'PASSWORD': os.getenv('DB_PASSWORD', 'Read@123'),
+        'HOST': os.getenv('DB_HOST', 'localhost'),  # Or the IP address of your PostgreSQL server
+        'PORT': os.getenv('DB_PORT', '5432'),
     }
 }
+
+INSTALLED_APPS += ['django_elasticsearch_dsl']
 
 AWS_ACCESS_KEY_ID = os.getenv('AWS_ACCESS_KEY_ID')
 AWS_SECRET_ACCESS_KEY = os.getenv('AWS_SECRET_ACCESS_KEY')
 AWS_STORAGE_BUCKET_NAME = os.getenv('AWS_STORAGE_BUCKET_NAME')
-AWS_S3_SIGNATURE_NAME = 's3v4'
-AWS_S3_REGION_NAME = os.getenv('AWS_S3_REGION_NAME')
-AWS_S3_FILE_OVERWRITE = False
-AWS_DEFAULT_ACL = None
-AWS_S3_VERIFY = True
+AWS_S3_REGION_NAME = os.getenv('AWS_S3_REGION_NAME', 'us-east-1')  
+AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
 
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-MEDIA_URL = f"https://{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com/media/"
+MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/'
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_DIRS = [BASE_DIR / "static"]
+STATIC_ROOT = os.path.join(BASE_DIR, 'collected_static')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
