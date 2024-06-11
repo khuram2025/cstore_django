df154fcddf85a650d94fb04b6279bea749734b7a
khuram2025 <khuram2025@gmail.com>
2024-06-11 05:34:34 +0300

    fixing

--- cstore/settings/prod.py
+++ cstore/settings/prod.py
@@ -19,8 +19,6 @@ DATABASES = {
     }
 }
 
-INSTALLED_APPS += ['django_elasticsearch_dsl']
-
 AWS_ACCESS_KEY_ID = os.getenv('AWS_ACCESS_KEY_ID')
 AWS_SECRET_ACCESS_KEY = os.getenv('AWS_SECRET_ACCESS_KEY')
 AWS_STORAGE_BUCKET_NAME = os.getenv('AWS_STORAGE_BUCKET_NAME')
